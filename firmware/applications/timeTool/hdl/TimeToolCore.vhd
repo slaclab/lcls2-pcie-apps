4a73f0afe09549a5cde5eed983a4774ca6fbd989
Ryan Herbst <rherbst@slac.stanford.edu>
2017-12-13 09:41:43 -0800

    added applicatins

---
+++ firmware/applications/timeTool/hdl/TimeToolCore.vhd
@@ -0,0 +1,182 @@
+-------------------------------------------------------------------------------
+-- File       : TimeToolCore.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-12-04
+-- Last update: 2017-12-04
+-------------------------------------------------------------------------------
+-- Description:
+-------------------------------------------------------------------------------
+-- This file is part of 'axi-pcie-core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'axi-pcie-core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.AxiPkg.all;
+use work.SsiPkg.all;
+use work.AxiPciePkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity TimeToolCore is
+   generic (
+      TPD_G            : time             := 1 ns;
+      AXI_ERROR_RESP_G : slv(1 downto 0)  := AXI_RESP_DECERR_C);
+   port (
+      -- System Interface
+      sysClk          : in    sl;
+      sysRst          : in    sl;
+      -- DMA Interfaces  (sysClk domain)
+      dataInMaster    : in    AxiStreamMasterType;
+      dataInSlave     : out   AxiStreamSlaveType;
+      dataOutMaster   : out   AxiStreamMasterType;
+      dataOutSlave    : in    AxiStreamSlaveType;
+      -- AXI-Lite Interface
+      axilReadMaster  : in    AxiLiteReadMasterType;
+      axilReadSlave   : out   AxiLiteReadSlaveType;
+      axilWriteMaster : in    AxiLiteWriteMasterType;
+      axilWriteSlave  : out   AxiLiteWriteSlaveType);
+end TimeToolCore;
+
+architecture mapping of TimeToolCore is
+
+   constant INT_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes=>16,tDestBits=>0);
+
+   type RegType is record
+      master          : AxiStreamMasterType;
+      slave           : AxiStreamSlaveType;
+      test            : slv(31 downto 0);
+      axilReadSlave   : AxiLiteReadSlaveType;
+      axilWriteSlave  : AxiLiteWriteSlaveType;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      master          => AXI_STREAM_MASTER_INIT_C,
+      slave           => AXI_STREAM_SLAVE_INIT_C,
+      test            => (others=>'0'),
+      axilReadSlave   => AXI_LITE_READ_SLAVE_INIT_C,
+      axilWriteSlave  => AXI_LITE_WRITE_SLAVE_INIT_C);
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
+   signal inMaster : AxiStreamMasterType;
+   signal inSlave  : AxiStreamSlaveType;
+   signal outCtrl  : AxiStreamCtrlType;
+
+begin
+
+   ---------------------------------
+   -- Input FIFO
+   ---------------------------------
+   U_InFifo: entity work.AxiStreamFifoV2
+      generic map (
+         TPD_G               => TPD_G,
+         SLAVE_READY_EN_G    => true,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 9,
+         FIFO_PAUSE_THRESH_G => 500,
+         SLAVE_AXI_CONFIG_G  => DMA_AXIS_CONFIG_C,
+         MASTER_AXI_CONFIG_G => INT_CONFIG_C)
+      port map (
+         sAxisClk    => sysClk,
+         sAxisRst    => sysRst,
+         sAxisMaster => dataInMaster,
+         sAxisSlave  => dataInSlave,
+         mAxisClk    => sysClk,
+         mAxisRst    => sysRst,
+         mAxisMaster => inMaster,
+         mAxisSlave  => inSlave);
+
+   ---------------------------------
+   -- Application
+   ---------------------------------
+   comb : process (r, sysRst, axilReadMaster, axilWriteMaster, inMaster, outCtrl) is
+      variable v      : RegType;
+      variable axilEp : AxiLiteEndpointType;
+   begin
+
+      -- Latch the current value
+      v := r;
+
+      ------------------------      
+      -- AXI-Lite Transactions
+      ------------------------      
+
+      -- Determine the transaction type
+      axiSlaveWaitTxn(axilEp, axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave);
+
+      axiSlaveRegister (axilEp, x"000",  0, v.test);
+
+      axiSlaveDefault(axilEp, v.axilWriteSlave, v.axilReadSlave, AXI_ERROR_RESP_G);
+
+      ------------------------------
+      -- Data Mover
+      ------------------------------
+      v.slave.tReady := not outCtrl.pause;
+
+      if v.slave.tReady = '1' and inMaster.tValid = '1' then
+         v.master := inMaster;
+      else
+         v.master.tValid := '0';
+      end if;
+
+      -------------
+      -- Reset
+      -------------
+      if (sysRst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+      -- Outputs 
+      axilReadSlave  <= r.axilReadSlave;
+      axilWriteSlave <= r.axilWriteSlave;
+      inSlave        <= v.slave;
+
+   end process comb;
+
+   seq : process (sysClk) is
+   begin
+      if (rising_edge(sysClk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+   ---------------------------------
+   -- Output FIFO
+   ---------------------------------
+   U_OutFifo: entity work.AxiStreamFifoV2
+      generic map (
+         TPD_G               => TPD_G,
+         SLAVE_READY_EN_G    => false,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 9,
+         FIFO_PAUSE_THRESH_G => 500,
+         SLAVE_AXI_CONFIG_G  => INT_CONFIG_C,
+         MASTER_AXI_CONFIG_G => DMA_AXIS_CONFIG_C)
+      port map (
+         sAxisClk    => sysClk,
+         sAxisRst    => sysRst,
+         sAxisMaster => r.Master,
+         sAxisCtrl   => outCtrl,
+         mAxisClk    => sysClk,
+         mAxisRst    => sysRst,
+         mAxisMaster => dataOutMaster,
+         mAxisSlave  => dataOutSlave);
+
+end mapping;
