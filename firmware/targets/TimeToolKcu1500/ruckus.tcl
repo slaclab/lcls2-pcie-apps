f63cb64c16b6d2aeb8407af8042babd28de1491e
Larry Ruckman <ruckman@slac.stanford.edu>
2018-09-26 09:02:55 -0700

    migrating to firmware/submodules/lcls2-pgp-fw-lib

--- firmware/targets/TimeToolKcu1500/ruckus.tcl
+++ firmware/targets/TimeToolKcu1500/ruckus.tcl
@@ -5,7 +5,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/axi-pcie-core
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/lcls-timing-core
-loadRuckusTcl $::env(PROJ_DIR)/../../common/pgp2b
+loadRuckusTcl $::env(PROJ_DIR)/../../submodules/lcls2-pgp-fw-lib/pgp2b-only
 loadRuckusTcl $::env(PROJ_DIR)/../../applications
 
 # Load local source Code and constraints
