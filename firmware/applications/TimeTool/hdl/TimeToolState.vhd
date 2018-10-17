477ba63f3a7a31328bcbcaaf88630f4840ad9d22
Sioan Zohar <sioan@slac.stanford.edu>
2018-10-17 14:26:13 -0700

    implementing state machine

---
+++ firmware/applications/TimeTool/hdl/TimeToolState.vhd
@@ -0,0 +1,262 @@
+------------------------------------------------------------------------------
+-- File       : TimeToolCore.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-12-04
+-- Last update: 2017-12-04
+-------------------------------------------------------------------------------
+-- Description:
+-------------------------------------------------------------------------------
+-- This file is part of 'axi-pcie-core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'axi-pcie-core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.AxiPkg.all;
+use work.SsiPkg.all;
+use work.AxiPciePkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+---------------------------------
+---- entity declaration----------
+---------------------------------
+
+entity TimeToolCore is
+   generic (
+      TPD_G            : time             := 1 ns;
+      AXI_ERROR_RESP_G : slv(1 downto 0)  := AXI_RESP_DECERR_C);
+   port (
+      -- System Interface
+      sysClk          : in    sl;
+      sysRst          : in    sl;
+      -- DMA Interfaces  (sysClk domain)
+      dataInMaster    : in    AxiStreamMasterType;
+      dataInSlave     : out   AxiStreamSlaveType;
+      dataOutMaster   : out   AxiStreamMasterType;
+      dataOutSlave    : in    AxiStreamSlaveType;
+      -- AXI-Lite Interface
+      axilReadMaster  : in    AxiLiteReadMasterType;
+      axilReadSlave   : out   AxiLiteReadSlaveType;
+      axilWriteMaster : in    AxiLiteWriteMasterType;
+      axilWriteSlave  : out   AxiLiteWriteSlaveType);
+end TimeToolCore;
+
+
+---------------------------------
+--------- architecture-----------
+---------------------------------
+
+architecture mapping of TimeToolCore is
+
+   constant INT_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes=>16,tDestBits=>0);
+
+   type StateType is (IDLE_S,MOVE_RAW_S,MOVE_FEATURE_EXTRACTED_S, MOVE_TIMESTAMP_S); 
+
+---------------------------------------
+----record for two process method------
+---------------------------------------
+
+   type RegType is record
+      master          : AxiStreamMasterType;
+      slave           : AxiStreamSlaveType;
+      addvalue        : slv(7 downto 0);
+      axilReadSlave   : AxiLiteReadSlaveType;
+      axilWriteSlave  : AxiLiteWriteSlaveType;
+      state           : StateType;
+   end record RegType;
+
+---------------------------------------
+-------record intitial value-----------
+---------------------------------------
+
+   constant REG_INIT_C : RegType := (
+      master          => AXI_STREAM_MASTER_INIT_C,
+      slave           => AXI_STREAM_SLAVE_INIT_C,
+      addValue        => (others=>'0'),
+      axilReadSlave   => AXI_LITE_READ_SLAVE_INIT_C,
+      axilWriteSlave  => AXI_LITE_WRITE_SLAVE_INIT_C,
+      state           => IDLE_S);
+
+---------------------------------------
+-------record intitial value-----------
+---------------------------------------
+
+
+   signal r   : RegType := REG_INIT_C;
+   signal rin : RegType;
+
+   signal inMaster : AxiStreamMasterType;
+   signal inSlave  : AxiStreamSlaveType;
+   signal outCtrl  : AxiStreamCtrlType;
+
+begin
+
+   ---------------------------------
+   -- Input FIFO
+   ---------------------------------
+   U_InFifo: entity work.AxiStreamFifoV2
+      generic map (
+         TPD_G               => TPD_G,
+         SLAVE_READY_EN_G    => true,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 9,
+         FIFO_PAUSE_THRESH_G => 500,
+         SLAVE_AXI_CONFIG_G  => DMA_AXIS_CONFIG_C,
+         MASTER_AXI_CONFIG_G => INT_CONFIG_C)
+      port map (
+         sAxisClk    => sysClk,
+         sAxisRst    => sysRst,
+         sAxisMaster => dataInMaster,
+         sAxisSlave  => dataInSlave,
+         mAxisClk    => sysClk,
+         mAxisRst    => sysRst,
+         mAxisMaster => inMaster,
+         mAxisSlave  => inSlave);
+
+   ---------------------------------
+   -- Application
+   ---------------------------------
+   comb : process (r, sysRst, axilReadMaster, axilWriteMaster, inMaster, outCtrl) is
+      variable v      : RegType;
+      variable axilEp : AxiLiteEndpointType;
+   begin
+
+      -- Latch the current value
+      v := r;
+
+      ------------------------      
+      -- AXI-Lite Transactions
+      ------------------------      
+
+      -- Determine the transaction type
+      axiSlaveWaitTxn(axilEp, axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave);
+
+      axiSlaveRegister (axilEp, x"000",  0, v.addValue);
+
+      axiSlaveDefault(axilEp, v.axilWriteSlave, v.axilReadSlave, AXI_ERROR_RESP_G);
+
+
+      v.master                :=     inMaster;
+
+
+       -- State Machine
+       --IDLE_S,MOVE_RAW_S,MOVE_FEATURE_EXTRACTED_S, MOVE_TIMESTAMP_S
+
+      case r.state is
+         ----------------------------------------------------------------------
+         when IDLE_S =>
+            -- Check if ready to move data
+            if inMaster.tValid = '1' then
+                  v.state := MOVE_RAW_S;
+            end if
+           --some logic
+           --next state          v.state := MOVE_S;
+      ----------------------------------------------------------------------
+         when MOVE_RAW_S => 
+            if prescale_counter < prescale_counter_increment and  v.slave.tReady = '1' then
+                  
+                  v.master.tLast          :=     '0';
+
+                  for i in 0 to INT_CONFIG_C.TDATA_BYTES_C-1 loop
+                        v.master.tData(i*8+7 downto i*8) := inMaster.tData(i*8+7 downto i*8) + r.addValue;
+                  end loop;
+
+            elsif prescale_counter < prescale_counter_increment and  v.slave.tReady = '0' then
+                  --do something to fail elegantly
+                  v.state := MOVE_RAW_S   --not necessary since two processes state is latched
+
+            elsif inMaster.tLast = '1' then
+                  v.state := MOVE_FEATURE_EXTRACTED_S
+
+            end if
+      ----------------------------------------------------------------------
+         when MOVE_FEATURE_EXTRACTED_S => 
+            v.master.tData(31 downto 0)   := x"DEAD";
+            v.master.tKeep                := x"000F";
+            v.master.tValid               := '1'; 
+            v.state := MOVE_TIMESTAMP_S;
+      ----------------------------------------------------------------------
+         when MOVE_TIMESTAMP_S => 
+            --some logic
+
+            v.master.tData(31 downto 0)   := r.pulseId;
+            v.master.tKeep                := x"000F";
+            v.master.tValid               := '1';                  
+            v.master.tLast                := '1';
+            v.state                       := IDLE_S;
+      ----------------------------------------------------------------------
+      end case;
+      -- Data Mover
+      ------------------------------
+      v.slave.tReady := not outCtrl.pause;
+
+      if v.slave.tReady = '1' and inMaster.tValid = '1' then
+         v.master := inMaster;
+
+         for i in 0 to INT_CONFIG_C.TDATA_BYTES_C-1 loop
+            v.master.tData(i*8+7 downto i*8) := inMaster.tData(i*8+7 downto i*8) + r.addValue;
+         end loop;
+
+      else
+         v.master.tValid := '0';
+      end if;
+
+      -------------
+      -- Reset
+      -------------
+      if (sysRst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+      -- Outputs 
+      axilReadSlave  <= r.axilReadSlave;
+      axilWriteSlave <= r.axilWriteSlave;
+      inSlave        <= v.slave;
+
+   end process comb;
+
+   seq : process (sysClk) is
+   begin
+      if (rising_edge(sysClk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+   ---------------------------------
+   -- Output FIFO
+   ---------------------------------
+   U_OutFifo: entity work.AxiStreamFifoV2
+      generic map (
+         TPD_G               => TPD_G,
+         SLAVE_READY_EN_G    => false,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 9,
+         FIFO_PAUSE_THRESH_G => 500,
+         SLAVE_AXI_CONFIG_G  => INT_CONFIG_C,
+         MASTER_AXI_CONFIG_G => DMA_AXIS_CONFIG_C)
+      port map (
+         sAxisClk    => sysClk,
+         sAxisRst    => sysRst,
+         sAxisMaster => r.Master,
+         sAxisCtrl   => outCtrl,
+         mAxisClk    => sysClk,
+         mAxisRst    => sysRst,
+         mAxisMaster => dataOutMaster,
+         mAxisSlave  => dataOutSlave);
+
+end mapping;
