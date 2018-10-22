1d8aeee6b0c000f05bd042c1088bcb5f6ba04c0f
Ryan Herbst <rherbst@slac.stanford.edu>
2018-10-22 12:15:42 -0700

    Update path

--- software/RateTest/setup_env_template.sh
+++ software/RateTest/setup_env_template.sh
@@ -2,7 +2,7 @@
 #source /afs/slac/g/reseng/rogue/v2.8.0/setup_env.sh
 #source /afs/slac/g/reseng/rogue/pre-release/setup_env.sh
 #source /afs/slac/g/reseng/rogue/master/setup_env.sh
-source setup_env.sh
+#source setup_env.sh
 
 # Package directories
 export SURF_DIR=${PWD}/../../firmware/submodules/surf/python/
@@ -11,7 +11,7 @@ export AXID_DIR=${PWD}/../../firmware/submodules/axi-pcie-core/python/
 export PGP_DIR=${PWD}/../../firmware/submodules/lcls2-pgp-fw-lib/python
 export LCLT_DIR=${PWD}/../../firmware/submodules/lcls-timing-core/python/
 
-export TTOOL_DIR=${PWD}/../../firmware/targets/TimeToolKcu1500/python
+export TTOOL_DIR=${PWD}/../../firmware/targets/RateTestKcu1500/python
 
 # Setup python path
 export PYTHONPATH=${SURF_DIR}:${AXID_DIR}:${PGP_DIR}:${LCLT_DIR}:${TTA_DIR}:${TTOOL_DIR}:${PYTHONPATH}
