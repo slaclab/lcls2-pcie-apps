77d236ff9ffa468a111fc61be90c82f999d84dea
Sioan Zohar <sioan@slac.stanford.edu>
2019-01-28 09:07:04 -0800

    created tb directory and modified ruckus tcl file accordingly.  behaves as expected for simulation. still need to validate synthesis

--- firmware/applications/TimeTool/ruckus.tcl
+++ firmware/applications/TimeTool/ruckus.tcl
@@ -4,3 +4,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load local Source Code and Constraints
 loadSource      -dir "$::DIR_PATH/hdl"
 loadIpCore -path "$::DIR_PATH/coregen/ila_0.xci"
+
+# Load Simulation
+loadSource -sim_only -dir "$::DIR_PATH/tb"
+
