148f538bd2e71fb4e27c736bc8654d3747a2f152
Sioan Zohar <sioan@slac.stanford.edu>
2019-01-29 09:22:31 -0800

    simulation for prescaler synthesizing and showing unexpected behavior.  May need to decode to confirm

---
+++ firmware/applications/TimeTool/tb/TimeToolPrescalerTB.vhd
@@ -0,0 +1,141 @@
+-------------------------------------------------------------------------------
+-- File       : TimeToolKcu1500.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-10-24
+-- Last update: 2018-11-08
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- This file is part of 'axi-pcie-dev'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'axi-pcie-dev', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.AxiPciePkg.all;
+use work.TimingPkg.all;
+use work.Pgp2bPkg.all;
+use work.SsiPkg.all;
+
+entity TimeToolPrescalerTB is end TimeToolPrescalerTB;
+
+architecture testbed of TimeToolPrescalerTB is
+
+   constant TPD_G             : time := 1 ns;
+   --constant BUILD_INFO_G      : BuildInfoType;
+
+   constant DMA_SIZE_C        : positive := 1;
+
+   constant NUM_AXI_MASTERS_C : positive := 2;
+   constant NUM_MASTERS_G     : positive := 2;
+
+   constant DMA_AXIS_CONFIG_C : AxiStreamConfigType := ssiAxiStreamConfig(16, TKEEP_COMP_C, TUSER_FIRST_LAST_C, 8, 2);  -- 16 byte (128-bit) AXIS interface
+   constant DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(16, TKEEP_COMP_C, TUSER_FIRST_LAST_C, 8, 2);
+
+   constant CLK_PERIOD_G : time := 10 ns;
+
+   constant SRC_CONFIG_C : AxiStreamConfigType := (
+      TSTRB_EN_C    => false,
+      TDATA_BYTES_C => 16, -- 128 bits
+      TDEST_BITS_C  => 0,
+      TID_BITS_C    => 0,
+      TKEEP_MODE_C  => TKEEP_COMP_C,
+      TUSER_BITS_C  => 2,
+      TUSER_MODE_C  => TUSER_FIRST_LAST_C);
+
+   signal userClk156   : sl;
+   signal dmaClk       : sl;
+   signal dmaRst       : sl;
+
+   signal appInMaster  : AxiStreamMasterType;
+   signal appInSlave   : AxiStreamSlaveType;
+   signal appOutMaster : AxiStreamMasterType;
+   signal appOutSlave  : AxiStreamSlaveType;
+
+   signal axilWriteMasters : AxiLiteWriteMasterArray(NUM_AXI_MASTERS_C-1 downto 0);
+   signal axilWriteSlaves  : AxiLiteWriteSlaveArray(NUM_AXI_MASTERS_C-1 downto 0);
+   signal axilReadMasters  : AxiLiteReadMasterArray(NUM_AXI_MASTERS_C-1 downto 0);
+   signal axilReadSlaves   : AxiLiteReadSlaveArray(NUM_AXI_MASTERS_C-1 downto 0);
+
+   signal axiClk   : sl;
+   signal axiRst   : sl;
+
+begin
+
+   appOutSlave.tReady <= '1';
+
+   --------------------
+   -- Clocks and Resets
+   --------------------
+   U_axilClk_2 : entity work.ClkRst
+      generic map (
+         CLK_PERIOD_G      => CLK_PERIOD_G,
+         RST_START_DELAY_G => 0 ns,
+         RST_HOLD_TIME_G   => 1000 ns)
+      port map (
+         clkP => dmaClk,
+         rst  => dmaRst);
+
+
+   --------------------
+   -- Clocks and Resets
+   --------------------
+   U_axilClk : entity work.ClkRst
+      generic map (
+         CLK_PERIOD_G      => CLK_PERIOD_G,
+         RST_START_DELAY_G => 0 ns,
+         RST_HOLD_TIME_G   => 1000 ns)
+      port map (
+         clkP => axiClk,
+         rst  => axiRst);
+
+   --------------------
+   -- Test data
+   --------------------  
+
+      U_PackTx : entity work.AxiStreamBytePackerTbTx
+         generic map (
+            TPD_G         => TPD_G,
+            BYTE_SIZE_C   => 2+1,
+            AXIS_CONFIG_G => SRC_CONFIG_C)
+         port map (
+            axiClk      => axiClk,
+            axiRst      => axiRst,
+            mAxisMaster => appInMaster);
+
+   --------------------
+   -- Modules to be tested
+   --------------------  
+
+
+   U_TimeToolPrescaler : entity work.TimeToolPrescaler
+      generic map (
+         TPD_G             => TPD_G,
+         DMA_AXIS_CONFIG_G => DMA_AXIS_CONFIG_G)
+      port map (
+         -- System Clock and Reset
+         sysClk          => dmaClk,
+         sysRst          => dmaRst,
+         -- DMA Interface (sysClk domain)
+         dataInMaster    => appInMaster,
+         dataInSlave     => appInSlave,
+         dataOutMaster   => appOutMaster,
+         dataOutSlave    => appOutSlave,
+         -- AXI-Lite Interface (sysClk domain)
+         axilReadMaster  => axilReadMasters(1),
+         axilReadSlave   => axilReadSlaves(1),
+         axilWriteMaster => axilWriteMasters(1),
+         axilWriteSlave  => axilWriteSlaves(1));
+
+
+end testbed;
