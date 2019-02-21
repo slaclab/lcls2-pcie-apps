94757d76dd07309fe840d21bed34f8c4b0427469
Sioan Zohar <sioan@slac.stanford.edu>
2019-02-21 09:05:13 -0800

    l2si-core needs to be commented out to synthesize (still wiating for results). some xilinx cores need to be re-generated for this board

--- firmware/targets/TimeToolKcu1500/ruckus.tcl
+++ firmware/targets/TimeToolKcu1500/ruckus.tcl
@@ -9,8 +9,8 @@ loadRuckusTcl $::env(PROJ_DIR)/../../submodules/l2si-core/base
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/axi-pcie-core/hardware/XilinxKcu1500
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/lcls2-pgp-fw-lib/pgp2b-only/hardware/XilinxKcu1500
 loadRuckusTcl $::env(PROJ_DIR)/../../applications
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/l2si-core/xpm
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/l2si-core/base
+#loadRuckusTcl $::env(PROJ_DIR)/../../submodules/l2si-core/xpm
+#loadRuckusTcl $::env(PROJ_DIR)/../../submodules/l2si-core/base
 
 # Load local source Code and constraints
 loadSource -dir "$::DIR_PATH/hdl"
