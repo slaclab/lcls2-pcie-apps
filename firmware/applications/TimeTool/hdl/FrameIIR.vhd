0f8f99753e16a9a530b9f6efaddc32c92bee0d27
Sioan Zohar <sioan@slac.stanford.edu>
2019-03-11 10:22:40 -0700

    progress on frame wise IIR filter

---
+++ firmware/applications/TimeTool/hdl/FrameIIR.vhd
@@ -0,0 +1,257 @@
+------------------------------------------------------------------------------
+-- File       : TimeToolCore.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2017-12-04
+-- Last update: 2017-12-04
+-------------------------------------------------------------------------------
+-- Description:
+-------------------------------------------------------------------------------
+-- This file is part of 'axi-pcie-core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'axi-pcie-core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.ALL;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.AxiPkg.all;
+use work.SsiPkg.all;
+use work.AxiPciePkg.all;
+use work.TimingPkg.all;
+use work.Pgp2bPkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+-------------------------------------------------------------------------------
+-- This file performs the the prescaling, or the amount of raw data which is stored
+-------------------------------------------------------------------------------
+
+entity FrameIIR is
+   generic (
+      TPD_G             : time                := 1 ns;
+      DMA_AXIS_CONFIG_G : AxiStreamConfigType := ssiAxiStreamConfig(16, TKEEP_COMP_C, TUSER_FIRST_LAST_C, 8, 2);
+      DEBUG_G           : boolean             := true );
+   port (
+      -- System Interface
+      sysClk          : in    sl;
+      sysRst          : in    sl;
+      -- DMA Interfaces  (sysClk domain)
+      dataInMaster    : in    AxiStreamMasterType;
+      dataInSlave     : out   AxiStreamSlaveType;
+      dataOutMaster   : out   AxiStreamMasterType;
+      dataOutSlave    : in    AxiStreamSlaveType;
+      -- AXI-Lite Interface
+      axilReadMaster  : in    AxiLiteReadMasterType;
+      axilReadSlave   : out   AxiLiteReadSlaveType;
+      axilWriteMaster : in    AxiLiteWriteMasterType;
+      axilWriteSlave  : out   AxiLiteWriteSlaveType);
+end FrameIIR;
+
+architecture mapping of FrameIIR is
+
+   constant INT_CONFIG_C                  : AxiStreamConfigType := ssiAxiStreamConfig(dataBytes=>16,tDestBits=>0);
+   constant PGP2BTXIN_LEN                 : integer             := 19;
+   constant CAMERA_RESOLUTION_BITS        : positive            := 8;
+   constant CAMERA_PIXEL_NUMBER           : positive            := 2048;
+
+   type CameraFrame is array (natural range<>) of slv(CAMERA_RESOLUTION_BITS-1 downto 0);
+
+   type StateType is (
+      IDLE_S,
+      MOVE_S,
+      UPDATE_S);
+
+   type RegType is record
+      master          : AxiStreamMasterType;
+      slave           : AxiStreamSlaveType;
+      axilReadSlave   : AxiLiteReadSlaveType;
+      axilWriteSlave  : AxiLiteWriteSlaveType;
+      counter         : natural range 0 to (CAMERA_PIXEL_NUMBER-1);
+      prescalingRate  : slv(31 downto 0);
+      axi_test        : slv(31 downto 0);
+      state           : StateType;
+   end record RegType;
+
+   constant REG_INIT_C : RegType := (
+      master          => AXI_STREAM_MASTER_INIT_C,
+      slave           => AXI_STREAM_SLAVE_INIT_C,
+      axilReadSlave   => AXI_LITE_READ_SLAVE_INIT_C,
+      axilWriteSlave  => AXI_LITE_WRITE_SLAVE_INIT_C,
+      counter         => 0,
+      prescalingRate  => (others=>'0'),
+      axi_test        => (others=>'0'),
+      state           => IDLE_S);
+
+---------------------------------------
+-------record intitial value-----------
+---------------------------------------
+
+
+   signal r             : RegType     := REG_INIT_C;
+   signal rin           : RegType;
+
+   signal rollingImage  : CameraFrame := (others => (others => '0'));
+
+   signal inMaster      : AxiStreamMasterType;
+   signal inSlave       : AxiStreamSlaveType;
+   signal outCtrl       : AxiStreamCtrlType;
+
+begin
+
+   ---------------------------------
+   -- Input FIFO
+   ---------------------------------
+   U_InFifo: entity work.AxiStreamFifoV2
+      generic map (
+         TPD_G               => TPD_G,
+         SLAVE_READY_EN_G    => true,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 9,
+         FIFO_PAUSE_THRESH_G => 500,
+         SLAVE_AXI_CONFIG_G  => DMA_AXIS_CONFIG_G,
+         MASTER_AXI_CONFIG_G => INT_CONFIG_C)
+      port map (
+         sAxisClk    => sysClk,
+         sAxisRst    => sysRst,
+         sAxisMaster => dataInMaster,
+         sAxisSlave  => dataInSlave,
+         mAxisClk    => sysClk,
+         mAxisRst    => sysRst,
+         mAxisMaster => inMaster,
+         mAxisSlave  => inSlave);
+
+
+   ---------------------------------
+   -- Application
+   ---------------------------------
+   comb : process (r, sysRst, axilReadMaster, axilWriteMaster, inMaster, outCtrl) is
+      variable v      : RegType;
+      variable axilEp : AxiLiteEndpointType;
+   begin
+
+      -- Latch the current value
+      v := r;
+
+      ------------------------      
+      -- AXI-Lite Transactions
+      ------------------------      
+
+      -- Determine the transaction type
+      axiSlaveWaitTxn(axilEp, axilWriteMaster, axilReadMaster, v.axilWriteSlave, v.axilReadSlave);
+
+      axiSlaveRegister (axilEp, x"00000", 0, v.prescalingRate);
+      --axiSlaveRegister (axilEp, x"00000", 8, v.axi_test);
+
+      axiSlaveDefault(axilEp, v.axilWriteSlave, v.axilReadSlave, AXI_RESP_DECERR_C);
+
+      v.slave.tReady    := not outCtrl.pause;
+      v.master.tLast    := '0';
+      v.master.tValid   := '0';
+
+      case r.state is
+
+            when IDLE_S =>
+            ------------------------------
+            -- check which state
+            ------------------------------
+            if v.slave.tReady = '1' and inMaster.tValid = '1' then
+                        v.state     := MOVE_S;
+               
+
+            else
+                  v.state           := IDLE_S;
+            end if;
+ 
+            --when MOVE_S =>
+                  ------------------------------
+                  -- send regular frame
+                  ------------------------------
+            --      if v.slave.tReady = '1' and inMaster.tValid = '1' then
+            --          v.master := inMaster;     --copies one 'transfer' (trasnfer is the AXI jargon for one TVALID/TREADY transaction)
+
+            --      else
+            --          v.master.tValid := '0';   --message to downstream data processing that there's no valid data ready
+            --          v.slave.tReady  := '0';   --message to upstream that we're not ready
+            --          v.master.tLast  := '0';
+            --          v.state         := IDLE_S;
+            --      end if;
+
+           
+            when UPDATE_S  => 
+                   ------------------------------
+                   -- update slv logic array
+                   ------------------------------
+
+               if v.slave.tReady = '1' and inMaster.tValid = '1' then
+                  v.master                   := inMaster;     --copies one 'transfer' (trasnfer is the AXI jargon for one TVALID/TREADY transaction)
+                  rollingImage(v.counter)    := (rollingImage[v.counter]/32)*31 + inMaster.tdata/32;
+                  v.master.tData             := rollingImage(v.counter);
+                  v.counter                  := v.counter+1;
+                      
+                  if v.slave.tLast := '1';
+                        v.counter            := 0;
+                  end if
+               else
+                  v.state                    := IDLE_S;
+               end if     
+
+      end case;
+
+      -------------
+      -- Reset
+      -------------
+      if (sysRst = '1') then
+         v := REG_INIT_C;
+      end if;
+
+      -- Register the variable for next clock cycle
+      rin <= v;
+
+      -- Outputs 
+      axilReadSlave  <= r.axilReadSlave;
+      axilWriteSlave <= r.axilWriteSlave;
+      inSlave        <= v.slave;
+
+   end process comb;
+
+   seq : process (sysClk) is
+   begin
+      if (rising_edge(sysClk)) then
+         r <= rin after TPD_G;
+      end if;
+   end process seq;
+
+   ---------------------------------
+   -- Output FIFO
+   ---------------------------------
+   U_OutFifo: entity work.AxiStreamFifoV2
+      generic map (
+         TPD_G               => TPD_G,
+         SLAVE_READY_EN_G    => false,
+         GEN_SYNC_FIFO_G     => true,
+         FIFO_ADDR_WIDTH_G   => 9,
+         FIFO_PAUSE_THRESH_G => 500,
+         SLAVE_AXI_CONFIG_G  => INT_CONFIG_C,
+         MASTER_AXI_CONFIG_G => DMA_AXIS_CONFIG_G)
+      port map (
+         sAxisClk    => sysClk,
+         sAxisRst    => sysRst,
+         sAxisMaster => r.Master,
+         sAxisCtrl   => outCtrl,
+         mAxisClk    => sysClk,
+         mAxisRst    => sysRst,
+         mAxisMaster => dataOutMaster,
+         mAxisSlave  => dataOutSlave);
+
+end mapping;
