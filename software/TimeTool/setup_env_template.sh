38d5b4bfef8aee53dac7fdd8322f1a2298cf3a2f
Larry Ruckman <ruckman@slac.stanford.edu>
2019-03-14 16:16:16 -0700

    adding submodules/clink-gateway-fw-lib

--- software/TimeTool/setup_env_template.sh
+++ software/TimeTool/setup_env_template.sh
@@ -8,6 +8,7 @@ export LCLT_DIR=${PWD}/../../firmware/submodules/lcls-timing-core/python
 export COM_DIR=${PWD}/../../firmware/submodules/lcls2-pgp-fw-lib/python
 export TTA_DIR=${PWD}/../../firmware/applications/TimeTool/python
 export TTOOL_DIR=${PWD}/../../firmware/targets/TimeToolKcu1500/python
+export FEB_DIR=${PWD}/../../firmware/targets/clink-gateway-fw-lib/python
 
 # Setup python path
-export PYTHONPATH=${SURF_DIR}:${AXID_DIR}:${LCLT_DIR}:${COM_DIR}:${TTA_DIR}:${TTOOL_DIR}:${PYTHONPATH}
+export PYTHONPATH=${SURF_DIR}:${AXID_DIR}:${LCLT_DIR}:${COM_DIR}:${TTA_DIR}:${TTOOL_DIR}:${FEB_DIR}:${PYTHONPATH}
