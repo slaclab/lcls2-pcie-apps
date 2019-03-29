7724491655cb1f537936eb2a59335b6bfe47da57
Sioan Zohar <sioan@slac.stanford.edu>
2019-03-29 14:29:58 -0700

    progress on integration testing

--- software/TimeTool/scripts/integrated_system_test.py
+++ software/TimeTool/scripts/integrated_system_test.py
@@ -16,5 +16,16 @@ cl.GenFrame[0]
 
 gen_frame_method = cl.GenFrame[0]
 
+cl.Application.AppLane[0].Prescale.DialInPreScaling.set(2)
 for i in range(10):
-	gen_frame_method()
+	#gen_frame_method()
+	cl.GenFrame[0]()
+
+
+#cl.ClinkFeb[0].UartPiranha4[0]._tx.sendString('ssf 7000')
+#cl.ClinkFeb[0].UartPiranha4[0]._tx.sendString('ssf 6000')
+#cl.ClinkFeb[0].ClinkTop.Channel[0].DropCount.get()
+#cl.Application.AppLane[0].Prescale.DialInPreScaling.set(254)
+#cl.Application.AppLane[0].Prescale.DialInPreScaling.set(124)
+#cl.Application.AppLane[0].Prescale.DialInPreScaling.set(125)
+#cl.ClinkFeb[0].ClinkTop.Channel[0].DropCount.get()
