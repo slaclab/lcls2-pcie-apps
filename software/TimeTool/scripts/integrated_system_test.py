82f0c1b97f42a295de92972e4f496c673ebdf33b
Sioan Zohar <sioan@slac.stanford.edu>
2019-05-08 09:38:36 -0700

    DEV: able to send custom user space generated data to vcs sim with GenUserFrame method

--- software/TimeTool/scripts/integrated_system_test.py
+++ software/TimeTool/scripts/integrated_system_test.py
@@ -81,10 +81,12 @@ cl.Application.AppLane[0].ByPass.ByPass.set(0x1)
 
 
 cl.StartRun()
-#IPython.embed()
+
+IPython.embed()
 
 if(cl.dev[0]=='sim'):
-    cl.GenFrame[0]()
+    p =  np.array(cl._frameGen[0].make_byte_array())
+    cl.GenUserFrame[0]()(p)
 else:
     cl.Hardware.Timing.Triggering.LocalTrig[0].EnableTrig.set(True)
 
@@ -94,8 +96,6 @@ cl.Application.AppLane[0].Prescale.DialInPreScaling.set(0)
 
 start_count = 0
 
-#IPython.embed()
-
 my_results_list = []
 
 for i in range(4):
@@ -103,8 +103,11 @@ for i in range(4):
             break
       print("counter = "+str(start_count))
 
+      #p =  np.array(cl._frameGen[0].make_byte_array())
+      p = (np.random.rand(300)*255).astype(np.uint8)
+
       if(cl.dev[0]=='sim'):
-            cl.GenFrame[0]()
+            cl.GenUserFrame[0]()(p)
 
       too_many_counter  = 0
       while(cl.TimeToolRx.frameCount.get()<start_count+1):
@@ -114,8 +117,6 @@ for i in range(4):
 
       start_count = cl.TimeToolRx.frameCount.get()
       my_mask = np.arange(36)
-
-      p =  np.array(cl._frameGen[0].make_byte_array())
 
       if(len(p)>100):
             my_mask = np.append(my_mask,np.arange(int(len(p)/2),int(len(p)/2)+36))
