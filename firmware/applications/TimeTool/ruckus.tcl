eef47e4e57f5c71dde92f7c9269e1ee75da8c14a
Sioan Zohar <sioan@slac.stanford.edu>
2019-05-17 11:14:34 -0700

    DEV: integrating xilinx FIR compiler into ruckus build system. fir_compiler_3 takes > 2 hours to compile with vivado system.

--- firmware/applications/TimeTool/ruckus.tcl
+++ firmware/applications/TimeTool/ruckus.tcl
@@ -4,6 +4,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load local Source Code and Constraints
 loadSource      -dir "$::DIR_PATH/hdl"
 # loadIpCore -path "$::DIR_PATH/coregen/ila_1.xci"
+loadIpCore -path "$::DIR_PATH/coregen/fir_compiler_3.xci"
 
 # Load Simulation
 loadSource -sim_only -dir "$::DIR_PATH/tb"
