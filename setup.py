81b49480b40f8eaf254772751d6043c16661bac0
Christopher O'Grady <cpo@lcls-pc83236.slac.stanford.edu>
2019-07-09 18:28:53 -0700

    add first-pass setup.py, getting ready for conda

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+pdirlist = ['firmware/submodules/surf/python',
+            'firmware/submodules/axi-pcie-core/python',
+            'firmware/submodules/lcls-timing-core/python',
+            'firmware/submodules/lcls2-pgp-fw-lib/python',
+            'firmware/applications/TimeTool/python',
+            'firmware/submodules/clink-gateway-fw-lib/python',
+            'software/TimeTool/python']
+
+pnamelist = ['surf','axipcie','LclsTimingCore','lcls2_pgp_fw_lib','TimeTool','ClinkFeb','TimeToolDev']
+
+for pname,pdir in zip(pnamelist,pdirlist):
+    setup(
+        name = pname,
+        license = 'LCLS II',
+        description = 'LCLS II firmware package',
+        package_dir = {'':pdir},
+        packages = find_packages(pdir),
+    )
