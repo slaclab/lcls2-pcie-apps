9fc85a2212ca5932fb87ee75d746e5067661fee7
Benjamin Reese <bareese@slac.stanford.edu>
2019-12-03 14:06:22 -0800

    Change instantiation order

--- firmware/applications/TimeTool/python/timetool/_TimeToolKcu1500.py
+++ firmware/applications/TimeTool/python/timetool/_TimeToolKcu1500.py
@@ -9,12 +9,11 @@ class TimeToolKcu1500(pr.Device):
     def __init__(self, numLanes=1, pgp3=False, **kwargs):
         super().__init__(**kwargs)
 
-        self.add(axipcie.AxiPcieCore(
-            offset      = 0x00000000,
-            numDmaLanes = numLanes,
-            expand      = False,
-        ))  
-        
+        # The time tool application
+        self.add(timetool.Application(
+            offset = 0x00C0_0000,
+            numLanes = numLanes))
+
             
         # PGP Hardware on PCIe 
         self.add(lcls2_pgp_fw_lib.hardware.XilinxKcu1500.Kcu1500Hsio( 
@@ -24,7 +23,10 @@ class TimeToolKcu1500(pr.Device):
             expand    = False,
         ))
 
-        # The time tool application
-        self.add(timetool.Application(
-            offset = 0x008C_0000,
-            numLanes = numLanes))
+        
+        self.add(axipcie.AxiPcieCore(
+            offset      = 0x00000000,
+            numDmaLanes = numLanes,
+            expand      = False,
+        ))  
+        
