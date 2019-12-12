026b1bc09d88a90a5a5a1ddb4e06e99910e23563
Sioan <zohar.sioan@gmail.com>
2019-12-11 16:21:43 -0800

    BUG: build script wasn't copied correctly

--- jenkins_build_shell_script.sh
+++ jenkins_build_shell_script.sh
@@ -1,13 +1,4 @@
-#somehow need to verify that /u1/psreldev directory exists.  Which is won't unless it's been made once.
-
-echo  "starting build script"
-
-echo  "setting builtin to exit when anything fails"
-set -e
-
-echo "export HOME=~"
-export HOME=~
-echo "export USER=`whoami`"#!/bin/bash
+#!/bin/bash
 
 #somehow need to verify that /u1/psreldev directory exists.  Which is won't unless it's been made once.
 
@@ -73,49 +64,3 @@ vivado -mode batch -source firmware/targets/TimeToolKcu1500/simulation.tcl
 #./build_all.sh
 #pytest psana/psana/tests
 #pytest psana/psana/tests/byhand_mpi.py
-export USER=`whoami`
-
-echo "source /reg/g/psdm/sw/conda2/manage/bin/psconda.sh"
-source /reg/g/psdm/sw/conda2/manage/bin/psconda.sh
-
-
-echo "activating conda environment with git-lfs"
-conda activate /reg/g/psdm/sw/conda2/inst/envs/ps-2.0.5/
-
-echo "doing the git clean -fxd"
-git clean -fxd
-
-echo "initializing submodules"
-git submodule init
-
-echo "updating (recursively)"
-git submodule update --recursive
-
-echo "generating simulated data"
-cd firmware/simulations/TimeToolKcu1500ComponentTb/data_generation_scripts
-python make_simulation_input_file.py
-cd ../../../../
-
-echo "sourcing the build environment"
-source firmware/setup_env_slac.sh
-
-echo "going to the time tool top directory to make"
-cd firmware/targets/TimeToolKcu1500
-
-echo "git lfs install --force"
-git lfs install --force
-echo "git-lfs pull"
-git-lfs pull
-echo "git submodule foreach git-lfs pull"
-git submodule foreach git-lfs pull
-
-
-echo "making clean"
-make clean
-
-echo "making no gui"
-make nogui
-
-echo "executing simulation script"
-cd ../../build/TimeToolKcu1500/
-vivado -mode batch -source firmware/targets/TimeToolKcu1500/simulation.tcl
