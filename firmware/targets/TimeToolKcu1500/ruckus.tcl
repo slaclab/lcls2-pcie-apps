1615fd991e6b8a5d86f907c395146526ee24b4f9
Benjamin Reese <bareese@slac.stanford.edu>
2020-02-07 10:48:32 -0800

    firmware/applications/TimeTool/ is now firmware/common/

--- firmware/targets/TimeToolKcu1500/ruckus.tcl
+++ firmware/targets/TimeToolKcu1500/ruckus.tcl
@@ -6,7 +6,7 @@ loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/lcls-timing-core
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/axi-pcie-core/hardware/XilinxKcu1500
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/lcls2-pgp-fw-lib/hardware/XilinxKcu1500
-loadRuckusTcl $::env(PROJ_DIR)/../../applications
+loadRuckusTcl $::env(PROJ_DIR)/../../common
 
 # Load the l2si-core source code
 loadSource -lib l2si_core -dir "$::env(PROJ_DIR)/../../submodules/l2si-core/xpm/rtl"
